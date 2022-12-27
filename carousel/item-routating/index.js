b5736a6ca882a0e66c3ab312019e4f998631a241
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2022-12-27 20:17:15 -0300

    stimulus.js logic in comments

--- carousel/item-routating/index.js
+++ carousel/item-routating/index.js
@@ -19,3 +19,32 @@ controls.forEach (control => {
     galeries[counter].classList.remove("d-none")
   })
 })
+
+// For stimulus.js
+
+// export default class extends Controller {
+//   static targets = ["gallery", "button"]
+//    static values = {
+//     interval: { type: Number, default: 0 },
+
+//   }
+//   connect() {
+//   }
+
+//   nextSlide() {
+//     this.galleryTargets[this.intervalValue].classList.add("d-none")
+//     const isLeft = event.currentTarget.classList.contains("arrow-left")
+//     if (isLeft) {
+//       this.intervalValue -= 1
+//     } else {
+//       this.intervalValue += 1
+//     }
+//     if (this.intervalValue >= this.galleryTargets.length) {
+//       this.intervalValue = 0
+//     }
+//     if (this.intervalValue < 0){
+//       this.intervalValue = this.galleryTargets.length - 1
+//     }
+//     this.galleryTargets[this.intervalValue].classList.remove("d-none")
+//   }
+// }
